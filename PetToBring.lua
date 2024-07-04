c7e64f9bfdce1a7061ec05d5424bc61e9cc11222
Niels Christian Skov Faber <nfab@itu.dk>
2024-07-04 17:33:18 +0200

    Basic window

---
+++ PetToBring.lua
@@ -0,0 +1,49 @@
+print("MyAddon successfully loaded!")
+
+-- Create the main frame
+local mainFrame = CreateFrame("Frame", "MyAddonMainFrame", UIParent, "BasicFrameTemplateWithInset")
+mainFrame:SetSize(500, 350)
+mainFrame:SetPoint("CENTER", UIParent, "CENTER", 0, 0)
+
+-- Customize the title background
+mainFrame.TitleBg:SetHeight(30)
+
+-- Create the title font string
+mainFrame.title = mainFrame:CreateFontString(nil, "OVERLAY", "GameFontHighlight")
+mainFrame.title:SetPoint("TOPLEFT", mainFrame.TitleBg, "TOPLEFT", 5, -3)
+mainFrame.title:SetText("PetToBring")
+mainFrame.playerName = mainFrame:CreateFontString(nil, "OVERLAY", "GameFontNormal")
+mainFrame.playerName:SetPoint("TOPLEFT", mainFrame, "TOPLEFT", 15, -35)
+mainFrame.playerName:SetText("Character: " .. UnitName("player") .. " (Level " .. UnitLevel("player") .. ")")
+mainFrame:Hide()
+table.insert(UISpecialFrames, "MyAddonMainFrame")
+
+-- Make the frame interactable and movable
+mainFrame:EnableMouse(true)
+mainFrame:SetMovable(true)
+mainFrame:RegisterForDrag("LeftButton")
+mainFrame:SetScript("OnDragStart", function(self)
+    self:StartMoving()
+end)
+mainFrame:SetScript("OnDragStop", function(self)
+    self:StopMovingOrSizing()
+end)
+
+-- Play sound on show and hide
+mainFrame:SetScript("OnShow", function(self)
+    PlaySound(808)
+end)
+mainFrame:SetScript("OnHide", function(self)
+    PlaySound(808)
+end)
+
+-- slash command
+SLASH_PETTOBRING1 = "/ptb"
+SLASH_PETTOBRING2 = "/pettobring"
+SlashCmdList["PETTOBRING"] = function()
+    if mainFrame:IsShown() then
+        mainFrame:Hide()
+    else
+        mainFrame:Show()
+    end
+end

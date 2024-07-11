ed9b2fb75538aa77a0211e2cbf7176a1cd3a5fd9
Niels Christian Skov Faber <nfab@itu.dk>
2024-07-12 00:01:41 +0200

    Reformatted some stuff

--- PetToBring.lua
+++ PetToBring.lua
@@ -1,5 +1,4 @@
-print("MyAddon successfully loaded!")
-if not MyAddonDB then 
+if not MyAddonDB then
     MyAddonDB = {}
 end
 
@@ -52,136 +51,133 @@ end
 local WowClasses = {
     ["Death Knight"] = {
         specs = {
-            { name = "Blood", icon = "Interface\\Icons\\Spell_Deathknight_BloodPresence" },
+            { name = "Blood",    icon = "Interface\\Icons\\Spell_Deathknight_BloodPresence" },
             { name = "Frost DK", icon = "Interface\\Icons\\Spell_Deathknight_FrostPresence" },
-            { name = "Unholy", icon = "Interface\\Icons\\Spell_Deathknight_UnholyPresence" }
+            { name = "Unholy",   icon = "Interface\\Icons\\Spell_Deathknight_UnholyPresence" }
         }
     },
     ["Druid"] = {
         specs = {
-            { name = "Balance", icon = "Interface\\Icons\\spell_nature_starfall" },
-            { name = "Feral", icon = "Interface\\Icons\\ability_druid_catform" },
-            { name = "Guardian", icon = "Interface\\Icons\\ability_racial_bearform" },
+            { name = "Balance",     icon = "Interface\\Icons\\spell_nature_starfall" },
+            { name = "Feral",       icon = "Interface\\Icons\\ability_druid_catform" },
+            { name = "Guardian",    icon = "Interface\\Icons\\ability_racial_bearform" },
             { name = "Restoration", icon = "Interface\\Icons\\spell_nature_healingtouch" }
         }
     },
     ["Hunter"] = {
         specs = {
             { name = "Beast Mastery", icon = "Interface\\Icons\\ability_hunter_beasttaming" },
-            { name = "Marksmanship", icon = "Interface\\Icons\\ability_hunter_focusedaim" },
-            { name = "Survival", icon = "Interface\\Icons\\ability_hunter_camouflage" }
+            { name = "Marksmanship",  icon = "Interface\\Icons\\ability_hunter_focusedaim" },
+            { name = "Survival",      icon = "Interface\\Icons\\ability_hunter_camouflage" }
         }
     },
     ["Mage"] = {
         specs = {
             { name = "Arcane", icon = "Interface\\Icons\\spell_holy_magicalsentry" },
-            { name = "Fire", icon = "Interface\\Icons\\spell_fire_firebolt02" },
-            { name = "Frost", icon = "Interface\\Icons\\spell_frost_frostbolt02" }
+            { name = "Fire",   icon = "Interface\\Icons\\spell_fire_firebolt02" },
+            { name = "Frost",  icon = "Interface\\Icons\\spell_frost_frostbolt02" }
         }
     },
     ["Paladin"] = {
         specs = {
-            { name = "Holy", icon = "Interface\\Icons\\spell_holy_holybolt" },
-            { name = "Protection", icon = "Interface\\Icons\\ability_paladin_shieldofthetemplar" },
+            { name = "Holy",        icon = "Interface\\Icons\\spell_holy_holybolt" },
+            { name = "Protection",  icon = "Interface\\Icons\\ability_paladin_shieldofthetemplar" },
             { name = "Retribution", icon = "Interface\\Icons\\spell_holy_auraoflight" }
         }
     },
     ["Priest"] = {
         specs = {
             { name = "Discipline", icon = "Interface\\Icons\\spell_holy_wordfortitude" },
-            { name = "Holy", icon = "Interface\\Icons\\spell_holy_guardianspirit" },
-            { name = "Shadow", icon = "Interface\\Icons\\spell_shadow_shadowwordpain" }
+            { name = "Holy",       icon = "Interface\\Icons\\spell_holy_guardianspirit" },
+            { name = "Shadow",     icon = "Interface\\Icons\\spell_shadow_shadowwordpain" }
         }
     },
     ["Rogue"] = {
         specs = {
             { name = "Assassination", icon = "Interface\\Icons\\ability_rogue_eviscerate" },
-            { name = "Combat", icon = "Interface\\Icons\\inv_sword_30" },
-            { name = "Subtlety", icon = "Interface\\Icons\\ability_stealth" }
+            { name = "Combat",        icon = "Interface\\Icons\\inv_sword_30" },
+            { name = "Subtlety",      icon = "Interface\\Icons\\ability_stealth" }
         }
     },
     ["Shaman"] = {
         specs = {
-            { name = "Elemental", icon = "Interface\\Icons\\spell_nature_lightning" },
+            { name = "Elemental",   icon = "Interface\\Icons\\spell_nature_lightning" },
             { name = "Enhancement", icon = "Interface\\Icons\\spell_nature_lightningshield" },
             { name = "Restoration", icon = "Interface\\Icons\\spell_nature_magicimmunity" }
         }
     },
     ["Warlock"] = {
         specs = {
-            { name = "Affliction", icon = "Interface\\Icons\\spell_shadow_deathcoil" },
-            { name = "Demonology", icon = "Interface\\Icons\\spell_shadow_metamorphosis" },
+            { name = "Affliction",  icon = "Interface\\Icons\\spell_shadow_deathcoil" },
+            { name = "Demonology",  icon = "Interface\\Icons\\spell_shadow_metamorphosis" },
             { name = "Destruction", icon = "Interface\\Icons\\spell_shadow_rainoffire" }
         }
     },
     ["Warrior"] = {
         specs = {
-            { name = "Arms", icon = "Interface\\Icons\\ability_warrior_savageblow" },
-            { name = "Fury", icon = "Interface\\Icons\\ability_warrior_innerrage" },
+            { name = "Arms",               icon = "Interface\\Icons\\ability_warrior_savageblow" },
+            { name = "Fury",               icon = "Interface\\Icons\\ability_warrior_innerrage" },
             { name = "Protection Warrior", icon = "Interface\\Icons\\inv_shield_06" }
         }
     }
 }
+
+local selectedSpecs = {}
 -- checkbtns
-local function createCheckButton(parent, xOff, yOff, text, texture)
+local function createCheckButton(parent, xOff, yOff, spec, texture)
     local checkButton = CreateFrame("CheckButton", nil, parent, "ChatConfigCheckButtonTemplate")
-        checkButton:SetPoint("TOPLEFT",mainFrame.TitleBg, "BOTTOMLEFT", xOff,yOff)
-        print("Button was created at: ", xOff,yOff)
-        checkButton.tooltip = text
-        local disabledTexture = checkButton:CreateTexture()
-        disabledTexture:SetTexture(texture)
-        disabledTexture:SetAllPoints()
-        disabledTexture:SetVertexColor(0.3, 0.3, 0.3, 1)  -- Make texture darker
-        checkButton:SetNormalTexture(disabledTexture)
-        checkButton:SetCheckedTexture(texture)
-        checkButton:SetHighlightTexture(texture)
-        return checkButton;
+    checkButton:SetPoint("TOPLEFT", parent, "TOPLEFT", xOff, yOff)
+    print("Button was created at: ", xOff, yOff)
+    --checkButton.tooltip = text
+    local disabledTexture = checkButton:CreateTexture()
+    disabledTexture:SetTexture(texture)
+    disabledTexture:SetAllPoints()
+    disabledTexture:SetVertexColor(0.3, 0.3, 0.3, 1) -- Make texture darker
+    checkButton:SetNormalTexture(disabledTexture)
+    checkButton:SetCheckedTexture(texture)
+    checkButton:SetHighlightTexture(texture)
+
+    checkButton:HookScript("OnClick", function()
+        if checkButton:GetChecked() then
+            table.insert(selectedSpecs, spec)
+        else
+            for i, v in ipairs(selectedSpecs) do
+                if v == spec then
+                    table.remove(selectedSpecs, i)
+                end
+            end
+        end
+    end);
+    return checkButton;
 end
 
 local xcord = 15
-local ycord = -20
+local ycord = -50
 local no = 0
-local selectedSpecs = {}
 
 for class, data in pairs(WowClasses) do
-    print(no, " ", class)
-    no = no+1
+    no = no + 1
     local classLabel = mainFrame:CreateFontString(nil, "OVERLAY", "GameFontNormal")
-    classLabel:SetPoint("TOPLEFT", xcord, ycord-15)
-    print("Setting label at: ", xcord, ycord-15)
+    classLabel:SetPoint("TOPLEFT", mainFrame, "TOPLEFT", xcord, ycord + 15)
     classLabel:SetText(class)
     for _, spec in pairs(data.specs) do
-    local myCheckButton = createCheckButton(mainFrame, xcord, ycord, spec.name,spec.icon);
-        print("creating checkbutton at: ", xcord, ycord)
-        myCheckButton.tooltip = spec.name;
-        myCheckButton:HookScript("OnClick", function()
-            if myCheckButton:GetChecked() then
-                table.insert(selectedSpecs,spec.name)
-            else
-                for i, v in ipairs (selectedSpecs) do
-                    if v == spec then
-                        table.remove(selectedSpecs,i)
-                    end
-                end
-            end
-        end);
+        createCheckButton(mainFrame, xcord, ycord, spec.name, spec.icon);
         xcord = xcord + 30
     end
-    --pls dont look at this it's an ugly way to make two columns 
-    --for the classes, 5 each
+
     if no == 5 then
         xcord = 115
-        ycord = -20
+        ycord = -50
     elseif no > 5 then
         xcord = 115
-        ycord = ycord -50
+        ycord = ycord - 50
     else
         xcord = 15
         ycord = ycord - 50
     end
 end
 
-function containsAny(table, required_specs)
+local function containsAny(table, required_specs)
     for _, item in ipairs(table) do
         for _, spec in ipairs(required_specs) do
             if item == spec then
@@ -197,17 +193,17 @@ calcbutton:SetPoint("BOTTOM", mainFrame, "BOTTOM", 0, 10)
 calcbutton:SetSize(100, 40)
 calcbutton:SetText("Find pet")
 calcbutton:SetScript("OnClick", function(self, button, down)
-	if not containsAny(selectedSpecs, {"Protection Warrior", "Guardian"}) then
+    if not containsAny(selectedSpecs, { "Protection Warrior", "Guardian" }) then
         print("Serpent or Raptor")
-    elseif not containsAny(selectedSpecs, {"Fury", "Elemental", "Subtlety", "Feral"}) then
+    elseif not containsAny(selectedSpecs, { "Fury", "Elemental", "Subtlety", "Feral" }) then
         print("Wolf or Devilsaur")
-    elseif not containsAny(selectedSpecs, {"Arms", "Combat", "Frost DK"}) then
+    elseif not containsAny(selectedSpecs, { "Arms", "Combat", "Frost DK" }) then
         print("Ravager")
-    elseif not containsAny(selectedSpecs, {"Assassination", "Unholy", "Balance", "Affliction", "Demonology", "Destruction"}) then
+    elseif not containsAny(selectedSpecs, { "Assassination", "Unholy", "Balance", "Affliction", "Demonology", "Destruction" }) then
         print("Wind Serpent or DragonHawk")
-    elseif not containsAny(selectedSpecs, {"Arms", "Fury", "Protection Warrior", "Enhancement", "Unholy", "Frost DK", "Blood"}) then
+    elseif not containsAny(selectedSpecs, { "Arms", "Fury", "Protection Warrior", "Enhancement", "Unholy", "Frost DK", "Blood" }) then
         print("Cat or spiritbeasts")
-    elseif not containsAny(selectedSpecs, {"Arms", "Feral", "Subtlety"}) then
+    elseif not containsAny(selectedSpecs, { "Arms", "Feral", "Subtlety" }) then
         print("Hyena")
     else
         print("Bring Whatever You want")
